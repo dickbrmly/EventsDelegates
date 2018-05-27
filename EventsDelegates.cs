e6ac099893af20504ba29bfddd956c66146b91a3
Richard Bromley <bromley@unlv.nevada.edu>
2018-05-27 10:14:28 -0700

    "Added Event"

--- EventsDelegates.cs
+++ EventsDelegates.cs
@@ -1,13 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EventsAndDelegates 
 {
+    //***************************************************************************************
+    //***************************************************************************************
+    //***************************************************************************************
+    //***************************************************************************************
     class EventObject
     {
         delegate void Mydel(string s);// A delegate that can hold functions 
                                       // to called in its invocation list.
+    //***************************************************************************************
         delegate void Anonymous();    // Anonymous method invocation.
-
+    //***************************************************************************************
         static void Main(string[] args)
         {
             Mydel del = TestFunction; // TestFunction added to invocation list.
@@ -25,11 +33,57 @@ namespace EventsAndDelegates
                 }
             };
 
+            var p = new EventAlarm(); //Publisher
+            var s = new MakeSound(); //Subscriber
+
+            p.sound += s.soundoff;  // 
+            p.sounder();
+
+         
+            MyAnon += () => System.Console.WriteLine("Lambda baby!"); //new invocation using 
+                                                                      // lambda expression
+
             MyAnon(); //call to anonymous delegate.
         }
+    //***************************************************************************************
         static void TestFunction(string text)
         {
             System.Console.WriteLine(text);
+        }
+    }
+    //***************************************************************************************
+    //***************************************************************************************
+    //***************************************************************************************
+    //***************************************************************************************
+    public class MakeSound
+    {
+        public void soundoff(object source, EventArgs e)
+        {
+            System.Console.Beep();
+        }
+    }
+    //***************************************************************************************
+    //***************************************************************************************
+    //***************************************************************************************
+    //***************************************************************************************
+    public class EventAlarm
+    {
+        /* 1 - Define a delegate
+           2 - Define an event based on that delegate.
+           3 - Raise the event. */
+        public delegate void MyEventHandler(object source, EventArgs args);
+        public event MyEventHandler sound; 
+        public void sounder()
+        {
+            onSoundRequest();
+        }
+   //***************************************************************************************
+        protected virtual void onSoundRequest()
+        {
+            if(sound != null)
+            {
+                sound(this,EventArgs.Empty);
+            }
         }
     }
 }
